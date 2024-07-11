7f0d0749fb3d74fb93bf22c61f0af284e179d593
tangoat <tanguy.soutric@eisti.fr>
2024-07-11 11:07:35 +0200

    started navbar

--- capsvm_app/main.js
+++ capsvm_app/main.js
@@ -1,36 +1,27 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow } = require("electron/main");
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
 
-let win;
+    win.setMenuBarVisibility(false);
+    win.maximize();
+    win.loadFile("capsvmApp.html");
+};
 
-function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600});
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+app.whenReady().then(() => {
+    createWindow();
 
-  win.webContents.openDevTools();
-  win.on('closed', () => {
-    win = null;
-  });
-}
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
 
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
         app.quit();
-    }
-    }
-);
-
-app.on('activate', () => {
-    if (win === null) {
-        createWindow();
     }
 });
