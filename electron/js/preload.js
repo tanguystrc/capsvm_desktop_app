a07d58141ae64c832fa96d28993d2d7c50cf9907
asmoz <gittonjuli@cy-tech.fr>
2024-07-17 14:39:37 +0200

    style + get the vm list of the server selected

--- electron/js/preload.js
+++ electron/js/preload.js
@@ -12,7 +12,21 @@ contextBridge.exposeInMainWorld("sqlite_server", {
 contextBridge.exposeInMainWorld('get_server_status', {
     sendPostRequest: async (serverUrl, endpoint, combinedToken) => {
         try {
-            let response = await ipcRenderer.invoke('send-post-request', serverUrl, endpoint, combinedToken);
+            let response = await ipcRenderer.invoke('status-request', serverUrl, endpoint, combinedToken);
+            console.error(response);
+            return response;
+        } catch (error) {
+            console.error('Erreur lors de l\'appel à status-request  : ', error);
+            throw error;
+        }
+    }
+});
+
+contextBridge.exposeInMainWorld('send_post_request', {
+    sendPostRequest: async (serverUrl, endpoint, combinedToken, postData) => {
+        try {
+            let response = await ipcRenderer.invoke('send-post-request', serverUrl, endpoint, combinedToken, postData);
+            console.error(response);
             return response;
         } catch (error) {
             console.error('Erreur lors de l\'appel à send-post-request  : ', error);
